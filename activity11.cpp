17cff0d6d7dae2166d225a67ed99cde7d125f5fb
MatteoBrownlow <114264680+MatteoBrownlow@users.noreply.github.com>
2024-09-18 16:16:04 -0700

    Update activity11.cpp

--- activity11.cpp
+++ activity11.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main(){
-
+  int num1, num2;
+  cout << "Enter the first value: " << endl;
+  cin >> num1;
+  cout << "Enter the second value: " << endl;
+  cin >> num2;
+  if(num1 > num2){
+    cout << num1;
+  }
+  else{
+    cout << num2;
+  }
+  return 0;
 }
